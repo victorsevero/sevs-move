54b1ab75b46b2e739756dd4b22fb6a2c0537e57b
Sevs <victor.severo.miranda@gmail.com>
2018-07-05 23:11:52 -0300

    Script para criação da pasta 'icons_with_bg' com ícones da pasta 'icons' colados por cima do background

--- icons_with_bg.py
+++ icons_with_bg.py
@@ -1,10 +1,15 @@
+import os
 from PIL import Image
 
-background = Image.open("bg.png")
-overlay = Image.open("001.png")
+path_icons = 'icons\\'
+path_bg = 'icons_with_bg\\'
+icons_dir = os.listdir(path_icons)
+train_dir = os.listdir(path_bg)
 
-background = background.convert("RGBA")
-overlay = overlay.convert("RGBA")
-
-new_img = Image.blend(background, overlay, 0.5)
-new_img.save("new.png","PNG")
+for file in icons_dir:
+    with open(path_icons + file, 'rb') as fp:
+        background = Image.open('bg.png')
+        icon = Image.open(fp).convert('RGBA')
+        x, y = icon.size
+        background.paste(icon, (0, 0, x, y), icon)
+        background.save(path_bg + '%s.png' %file[0:-4], format='png')
